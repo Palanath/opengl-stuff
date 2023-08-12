c1f8522078cdf67e3370c908b4832d2015b0917d
Palanath <pala@dusttoash.org>
2023-08-12 18:08:19 -0500

    Set up window and rendering pipeline

--- instanced-rendering/src/Instanced Rendering.cpp
+++ instanced-rendering/src/Instanced Rendering.cpp
@@ -3,5 +3,14 @@
 #include <GLUtils.hpp>
 
 int main() {
+	GLFWwindow *win = initOpenGL(1200, 800, "Instanced Rendering");
+	squareViewport(win);
+	glfwSetWindowSizeCallback(win, squareViewportGLFWCallback);
 
+	while (!glfwWindowShouldClose(win)) {
+		glfwPollEvents();
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(win);
+	}
 }
