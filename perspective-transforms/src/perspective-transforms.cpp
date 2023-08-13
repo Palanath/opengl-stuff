91741223963bc99eacc325f65e6cb748e18f6788
Palanath <pala@dusttoash.org>
2023-08-13 13:49:10 -0500

    Update perspective-transforms.cpp

--- perspective-transforms/src/perspective-transforms.cpp
+++ perspective-transforms/src/perspective-transforms.cpp
@@ -1,13 +1,20 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 #include <GLUtils.hpp>
+#include <pala/opengl/tests/multiple-vaos/Model.h>
 
 int main() {
 	GLFWwindow *win = initOpenGL(1200, 800, "Perspective Transforms");
 	glfwSetWindowSizeCallback(win, squareViewportGLFWCallback);
 	squareViewport(win);
 
+	pala::opengl::tests::multiple_vaos::Model cube;
+
+
 	while (!glfwWindowShouldClose(win)) {
 		glfwPollEvents();
 		glClear(GL_COLOR_BUFFER_BIT);
+
 
 		glfwSwapBuffers(win);
 	}
