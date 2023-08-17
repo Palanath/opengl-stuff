16168a9fb4943d235c97bcbe4d2444520b8c06e0
Palanath <pala@dusttoash.org>
2023-08-17 18:38:47 -0500

    Make cube render normally

--- perspective-transforms/src/perspective-transforms.cpp
+++ perspective-transforms/src/perspective-transforms.cpp
@@ -9,12 +9,90 @@ int main() {
 	squareViewport(win);
 
 	pala::opengl::tests::multiple_vaos::Model cube;
+	float data[] = {
+	// Front Face - 1
+			-.5, .5, 0, 1, 0, 0, 				//v1
+			.5, .5, 0, 0, 1, 0, 				//v2
+			-.5, -.5, 0, 0, 0, 1, 				//v3
+			// Front Face - 2
+			.5, .5, 0, 0, 1, 0,					//v2
+			-.5, -.5, 0, 0, 0, 1,				//v3
+			.5, -.5, 0, 1, 1, 0,				//v4
 
+			// Back Face - 1
+			-.5, .5, -1, 1, 0, 0,				//v5
+			.5, .5, -1, 0, 1, 0,				//v6
+			-.5, -.5, -1, 0, 0, 1,				//v7
+			// Back Face - 2
+			.5, .5, -1, 0, 1, 0,				//v6
+			-.5, -.5, -1, 0, 0, 1,				//v7
+			.5, -.5, -1, 1, 1, 0,				//v8
+
+			// Left Side - 1
+			-.5, .5, 0, 1, 0, 0,				//v1
+			-.5, .5, -1, 1, 0, 0,				//v5
+			-.5, -.5, 0, 0, 0, 1,				//v3
+			// Left Side - 2
+			-.5, .5, -1, 1, 0, 0,				//v5
+			-.5, -.5, 0, 0, 0, 1,				//v3
+			-.5, -.5, -1, 0, 0, 1,				//v7
+
+			// Top Side - 1
+			-.5, .5, 0, 1, 0, 0,				//v1
+			-.5, .5, -1, 1, 0, 0,				//v5
+			.5, .5, -1, 0, 1, 0,				//v6
+			// Top Side - 2
+			-.5, .5, -1, 1, 0, 0,				//v5
+			.5, .5, -1, 0, 1, 0,				//v6
+			.5, .5, 0, 0, 1, 0,					//v2
+
+			// Right Side - 1
+			.5, .5, 0, 0, 1, 0,					//v2
+			.5, .5, -1, 0, 1, 0,				//v6
+			.5, -.5, 0, 1, 1, 0,				//v4
+			// Right Side - 2
+			.5, .5, -1, 0, 1, 0,				//v6
+			.5, -.5, 0, 1, 1, 0,				//v4
+			.5, -.5, -1, 1, 1, 0,				//v8
+
+			// Bottom Side - 1
+			-.5, -.5, 0, 0, 0, 1,				//v3
+			.5, -.5, 0, 1, 1, 0,				//v4
+			-.5, -.5, -1, 0, 0, 1,				//v7
+			// Bottom Side - 2
+			.5, -.5, 0, 1, 1, 0,				//v4
+			-.5, -.5, -1, 0, 0, 1,				//v7
+			.5, -.5, -1, 1, 1, 0,				//v8
+			};
+	cube.addData(data, sizeof data);
+	cube.specifyAttribute(0, 0, 3, 6, 0);// Position
+	cube.specifyAttribute(1, 0, 3, 6, 3);// Color
+
+	auto prog = createShaderProgram(R"(#version 330 core
+
+in vec3 pos;
+in vec3 col;
+
+out vec3 color;
+
+void main() {
+	color = col;
+	gl_Position = vec4(pos, 1);
+})", R"(#version 330 core
+
+in vec3 color;
+out vec3 outcol;
+
+void main() {
+	outcol = color;
+})");
+	glUseProgram(prog);
 
 	while (!glfwWindowShouldClose(win)) {
 		glfwPollEvents();
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		cube.draw(36);
 
 		glfwSwapBuffers(win);
 	}
